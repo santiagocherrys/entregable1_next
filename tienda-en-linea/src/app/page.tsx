ed32e5b269ef7d42af5e7ebe9b91a98580225fc9
Santiago Echevery Serna <santiagocherrys@hotmail.com>
2024-08-26 12:32:12 -0500

    Added all the pictures and generated structure of CardItem

--- tienda-en-linea/src/app/page.tsx
+++ tienda-en-linea/src/app/page.tsx
@@ -1,7 +1,33 @@
 import Image from "next/image";
+import CardItem from "@/components/Cards";
 
 export default function Home() {
+
+  // const user = {
+  //   name: "John Doe",
+  //   age: 30,
+  //   email: "john.doe@example.com",
+  // };
+
+  const users: User[] = [
+    { image: "image1.png", title: "Ribbed Tank Top", price: 16.95 , colors: ["bg-orange-500","bg-black","bg-white"] },
+    { image: "image2.png", title: "Ribbed modal T-shirt", price: 18.95, colors: ["bg-yellow-700","bg-violet-500","bg-cyan-200"]},
+    { image: "image3.png", title: "Oversized Printed T-shirt", price: 10.00, colors: [] },
+    { image: "image4.png", title: "Oversized Printed T-shirt", price: 16.95, colors: ["bg-white-500","bg-violet-500","bg-black"] },
+    { image: "image5.png", title: "V-neck linen T-shirt", price: 114.95, colors: ["bg-yellow-800"] },
+    { image: "image6.png", title: "Loose Fit Sweatshirt", price: 10.00, colors: ["bg-teal-500","bg-black","bg-neutral-300", "bg-blue-500","bg-white","bg-gray-300"] },
+    { image: "image7.png", title: "Regular Fit Oxford Shirt", price: 10.00, colors: ["bg-black","bg-blue","bg-yellow-800","bg-sky-200"] },
+    { image: "image8.png", title: "Loose Fit Hoodie", price: 9.9, colors: ["bg-white","bg-black","bg-neutral-300"] },
+    
+  ];
   return (
-    <h1> Hola Mundo</h1>
+    <>
+    <div className="flex flex-wrap gap-5">
+      {users.map((user, index) => (
+        <CardItem key={index} user={user} />
+      ))}
+    </div>
+    </>
+    
   );
 }
---
+++ tienda-en-linea/src/components/Cards.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+interface User{
+    image: string;
+    title: number;
+    price: string;
+    colors: string[];
+}
+
+interface UserProfileProps {
+    user: User;
+  }
+export default function CardItem({user}: UserProfileProps){
+    return(<>
+    <div className="max-w-fit bg-white border border-gray-200 rounded-lg shadow ">
+        <Image src={`/${user.image}`} alt="dexter" width={300} height={300}></Image>
+         <h2 className="text-gray-700 font-bold">{user.title}</h2>
+         <h2 className="text-gray-700 font-bold">${user.price}</h2>
+        <div className="flex mt-4">
+            {user.colors.map((color, index) => (
+            <div>
+
+                 {/* First selection, this is the color in the picture */}
+                <div className={`w-6 h-6 rounded-full border-2 border-black flex items-center justify-center mr-2 ${index === 0 ? color : 'hidden'}`}>
+                <div className={`w-4 h-4 rounded-full ${color}`} ></div>
+                </div>
+                 {/* Others options */}
+                <div className={`w-6 h-6 rounded-full border-2 border-gray-300 ${index !== 0 ? color : 'hidden'} mr-2`}></div>
+            </div>
+            ))}
+        </div>
+        
+    </div>
+
+    </>);
+}
